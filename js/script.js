fe85023b98eb13888b72c3ce272ea710c0070368
MichaelLinch90 <michaellinch90@gmail.com>
2022-08-05 15:35:05 -0700

    defined players

--- js/script.js
+++ js/script.js
@@ -3,3 +3,13 @@ let currentPlayer;
 let selections;
 let winner;
 
+const player = {
+    '1' : {
+        name: 'p1',
+        color: 'red',
+    },
+    '-1' : {
+        name: 'p2',
+        color: 'yellow'
+    }
+}; 
